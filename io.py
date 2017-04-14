edc6e818594062bdfb354ae9bab22573c2a43fb2
ekohilas <ekohilas@users.noreply.github.com>
2017-04-15 02:32:48 +1000

    simplified input

--- io.py
+++ io.py
@@ -1,17 +1,9 @@
-from sys import argv
-
-def write_cases(func, filename):
-    with open(filename) as fi, open(filename.split(".")[0] + ".out", "w") as fo:
-        for i in range(1, int(fi.readline().strip())+1):
-            line = func(int(fi.readline().strip()))
-            print("Case #{}: {}".format(i, line), file=fo)
-
-def print_cases(func, filename):
-    with open(filename) as fi:
-        for i in range(1, int(fi.readline().strip())+1):
-            line = fi.readline().strip()
-            print("{} -> {}".format(line, func(int(line))))
+def print_cases(func):
+    for i in range(1, int(input())+1):
+        line = input()
+        output = func()
+        print("Case #{}: {}".format(i, output))
 
 if __name__ == "__main__":
-    #write_cases(, argv[1])
+    #print_cases()
 
